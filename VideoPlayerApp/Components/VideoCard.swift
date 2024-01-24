2f98166d04147472869062225e877d3e91c0e7ae
prajwalnaik28@gmail.com <prajwalnaik28@gmail.com>
2024-01-24 20:35:10 +0530

    Added modelData to read APi

---
+++ VideoPlayerApp/Components/VideoCard.swift
@@ -0,0 +1,18 @@
+//
+//  VideoCard.swift
+//  VideoPlayerApp
+//
+//  Created by Prajwal U on 21/01/24.
+//
+
+import SwiftUI
+
+struct VideoCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoCard()
+}
---
+++ VideoPlayerApp/Preview Content/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  VideoPlayerApp
+//
+//  Created by Prajwal U on 24/01/24.
+//
+
+import Foundation
